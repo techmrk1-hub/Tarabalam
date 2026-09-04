d80343d07f282d4274e635677631029ba92f4bd6
Ravikiran Mahankali <tech.mrk1@gmail.com>
2026-09-04 01:36:16 -0400

    Remove unused site.js

--- bramha-ui-v10/assets/site.js
+++
@@ -1,8 +0,0 @@
-(function(){
- const nav=document.querySelector('.primary-nav');
- const btn=document.querySelector('.mobile-menu-btn');
- btn?.addEventListener('click',()=>nav.classList.toggle('open'));
- document.querySelectorAll('.nav-item.has-menu>.nav-link').forEach(a=>a.addEventListener('click',e=>{
-   if(innerWidth<=760){e.preventDefault();a.parentElement.classList.toggle('open');}
- }));
-})();
