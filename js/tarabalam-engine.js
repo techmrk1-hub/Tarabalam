213363536285775a392bcbcca01b20a65b9135f3
Ravikiran Mahankali <tech.mrk1@gmail.com>
2026-09-05 21:39:41 -0400

    Restore full Tarabalam engine with te/en/hi/ta/kn language packs

--- js/tarabalam-engine.js
+++ js/tarabalam-engine.js
@@ -1,1 +1,10 @@
-PLACEHOLDER_LOAD_FROM_FILE:/tmp/engine-create-or-update.json
+    let currentLang = 'en';
+    let slideIndex = 0;
+    const totalSlides = 6;
+
+    // Integrated Five-Language Localized Matrix Schema Registry
+    const languagesDataset = {
+        te: {
+            appTitle: "\u0c24\u0c3e\u0c30\u0c3e\u0c2c\u0c32\u0c02 \u0c17\u0c23\u0c28 \u0c2a\u0c1f\u0c4d\u0c1f\u0c3f\u0c15 \u0c2e\u0c30\u0c3f\u0c2f\u0c41 \u0c38\u0c2e\u0c17\u0c4d\u0c30 \u0c38\u0c42\u0c1a\u0c3f\u0c15"
+        }
+    };
